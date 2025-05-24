99643323799f16c811d8adc2f02072cb31aa0622
Gabriel Albino <gabriel.albino@aluno.ifsp.edu.br>
2025-05-24 12:28:15 -0300

    dados mockados

--- drogaria/src/main/java/br/edu/ifsp/arq/controller/CreateMedicamentoServlet.java
+++ drogaria/src/main/java/br/edu/ifsp/arq/controller/CreateMedicamentoServlet.java
@@ -55,7 +55,24 @@ public class CreateMedicamentoServlet extends HttpServlet {
             request.setAttribute("classAlert", "p-0 alert alert-success");
 
             getServletContext().setAttribute("listaMedicamentos", dao.getMedicamentos());
+            
+            if(medicamento.getId() == 1) {
+                List<Medicamento> mocks = new ArrayList<>();
+                mocks.add(new Medicamento("Paracetamol", "Paracetamol", "Medley", "2025-12-31", "L001", "Dor e febre", "500mg", "Comprimido", 5.99, "images/paracetamol.png"));
+                mocks.add(new Medicamento("Dipirona", "Dipirona Monoidratada", "Neo Química", "2026-06-15", "L002", "Analgésico", "1g", "Comprimido", 4.50, "images/dipirona.png"));
+                mocks.add(new Medicamento("Ibuprofeno", "Ibuprofeno", "Bayer", "2026-01-20", "L003", "Inflamação e dor", "600mg", "Comprimido", 7.90, "images/ibuprofeno.png"));
+                mocks.add(new Medicamento("Amoxicilina", "Amoxicilina", "EMS", "2025-11-10", "L004", "Infecção bacteriana", "500mg", "Cápsula", 12.75, "images/amoxicilina.png"));
+                mocks.add(new Medicamento("Loratadina", "Loratadina", "Medquímica", "2026-04-01", "L005", "Alergia", "10mg", "Comprimido", 3.80, "images/loratadina.png"));
+                mocks.add(new Medicamento("Omeprazol", "Omeprazol", "Eurofarma", "2026-02-14", "L006", "Refluxo", "20mg", "Cápsula", 6.20, "images/omeprazol.png"));
+                mocks.add(new Medicamento("Salbutamol", "Salbutamol", "GSK", "2025-09-30", "L007", "Asma", "100mcg", "Spray", 15.30, "images/salbutamol.png"));
+                mocks.add(new Medicamento("Metformina", "Metformina", "Teuto", "2026-03-22", "L008", "Diabetes tipo 2", "850mg", "Comprimido", 8.60, "images/metformina.png"));
+                mocks.add(new Medicamento("Cetoconazol", "Cetoconazol", "Prati-Donaduzzi", "2025-10-05", "L009", "Infecções fúngicas", "200mg", "Comprimido", 9.45, "images/cetoconazol.png"));
+                mocks.add(new Medicamento("Azitromicina", "Azitromicina", "EMS", "2026-07-18", "L010", "Infecção respiratória", "500mg", "Comprimido", 18.99, "images/azitromicina.png"));
 
+                for (Medicamento m : mocks) {
+                    dao.adicionarMedicamento(m);
+                }
+            }
         } else {
             msg = "Erro ao cadastrar o medicamento.";
             request.setAttribute("mensagem", msg);
